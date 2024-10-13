7dde5962f3d62aaf9566b94278e2f8f491a316ae
Minh Bui <minhbui@ecellvm-05.engr.ucsb.edu>
2024-10-13 02:16:22 -0700

    finished S type and I types in the main controller

--- main_ctrl.sv
+++ main_ctrl.sv
@@ -30,118 +30,77 @@ module main_ctrl (
 			7'b0110011: begin
 				sel_a = 0;
 				sel_b = 0;
+				reg_wr = 1;
+				sel_wb = 1;
 				case (inst.funct3)
 					// ADD or SUB
-					3'b000:
-						alu_op = !inst.funct7 ? 0 : 4'b1001;
+					3'b000: alu_op = !inst.funct7 ? 0 : 4'b1001;
 					// XOR
-					3'b100:
-						alu_op = 4'b0001;
+					3'b100: alu_op = 4'b0001;
 					// OR
-					3'b110:
-						alu_op = 4'b0010;
+					3'b110: alu_op = 4'b0010;
 					// AND
-					3'b111:
-						alu_op = 4'b0011;
+					3'b111: alu_op = 4'b0011;
 					// SLL
-					3'b001:
-						alu_op = 4'b0100;
+					3'b001: alu_op = 4'b0100;
 					// SRL or SRA
-					3'b101:
-						alu_op = (!inst.funct7) ? 4'b0101 : 4'b0110; 
+					3'b101: alu_op = (!inst.funct7) ? 4'b0101 : 4'b0110; 
 					// SLT
-					3'b010:
-						alu_op = 4'b0111;
+					3'b010: alu_op = 4'b0111;
 					// SLTU
-					3'b011:
-						alu_op = 4'b1000;
+					3'b011: alu_op = 4'b1000;
 					default: alu_op = 0;
 				endcase	
 			end
 			// I arithmetic types
-			7'b0110011: begin
+			7'b0010011: begin
 				sel_a = 0;
 				sel_b = 1;
+				reg_wr = 1;
+				sel_wb = 1;
 				case (inst.funct3)
 					// ADD or SUB
-					3'b000:
-						alu_op = 0;
+					3'b000: alu_op = 0;
 					// XOR
-					3'b100:
-						alu_op = 4'b0001;
+					3'b100: alu_op = 4'b0001;
 					// OR
-					3'b110:
-						alu_op = 4'b0010;
+					3'b110: alu_op = 4'b0010;
 					// AND
-					3'b111:
-						alu_op = 4'b0011;
+					3'b111: alu_op = 4'b0011;
 					// SLL
-					3'b001:
-						alu_op = 4'b0100;
+					3'b001: alu_op = 4'b0100;
 					// SRL or SRA
-					3'b101:
-						alu_op = (!inst[31:26]) ? 4'b0101 : 4'b0110; 
+					3'b101: alu_op = (!inst[31:26]) ? 4'b0101 : 4'b0110; 
 					// SLT
-					3'b010:
-						alu_op = 4'b0111;
+					3'b010: alu_op = 4'b0111;
 					// SLTU
-					3'b011:
-						alu_op = 4'b1000;
+					3'b011: alu_op = 4'b1000;
 					default: alu_op = 0;
 				endcase	
 			end
 			// I - Load types
 			7'b0000011: begin
-				case (inst.funct3) 
-					// LB
-					3'b000: begin
-
-					end
-					// LH
-					3'b001: begin
-
-					end
-					// LW
-					3'b010: begin
-
-					end
-					// LBU
-					3'b100: begin
-
-					end
-					// LHU
-					3'b101: begin
-
-					end
-					default: begin
-					end
-				endcase
+				mem_rd = 1;
+				reg_wr = 1;
+				sel_wb = 0;
+				sel_b = 1;
+				mask = inst.funct3;
 			end
 			// S types
 			7'b0100011: begin
-				case (inst.funct3)
-					//SB
-					3'b000: begin
-
-					end
-					//SH
-					3'b001: begin
-
-					end
-					//SW
-					3'b010: begin
-
-					end
-					default: begin
-
-
-					end
-				endcase	
-				
+				mem_wr = 1;
+				sel_b = 1;	
+				mask = inst.funct3;	
 			end
 			default: begin
 				sel_a = 0;
 				sel_b = 0;
+				sel_wb = 0;
+				mem_wr = 0;
+				mem_rd = 0;
+				mask = 0;
+				alu_op = 0;
+				reg_wr = 0;
 			end
 		endcase
   end
