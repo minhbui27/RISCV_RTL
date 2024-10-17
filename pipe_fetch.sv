5c3993cf317098d30bb24f023c62b5dd494a0a8a
Cameron Barrett <cameronbarret@ucsb.edu>
2024-10-17 16:04:59 -0700

    finished pipe_fetch

--- pipe_fetch.sv
+++ pipe_fetch.sv
@@ -2,24 +2,46 @@
 
 module pipe_fetch(
 	input logic			clk,
-	input logic			reset,
+	input logic			rst,
+	input logic			flush,
+	input logic			stall,
 	input logic [31:0]	INST_F,
 	input logic	[31:0]	PC_F,
 	input logic	[31:0]	PC4_F,
 	output logic [31:0] INST_D,
 	output logic [31:0] PC_D,
-	output logic [31:0] PC4_D
+	output logic [31:0] PC4_D,
 );
+	// flip-flops to buffer signals during flushes
+	logic INST_buff_d[31:0], INST_buff_q[31:0]
+	logic PC_buff_d[31:0], PC_buff_q[31:0]
+	logic PC4_buff_d[31:0], PC4_buff_q[31:0]
 
-always_ff @(posedge clk, posedge reset)
-	if (reset) begin
-		inst_d <= 0;
-		pc_d <= 0;
-		pc_plus_4_d <= 0;
+	always_ff @(posedge clk, posedge rst) begin
+		if (rst)
+			INST_buff_q <= '0;
+			PC_buff_q <= '0;
+			PC4_buff_q <= '0;
+		else begin
+			INST_buff_q <= INST_buff_d;
+			PC_buff_q <= PC_buff_d;
+			PC4_buff_q <= PC4_buff_d;
+		end
 	end
-	else begin
-		inst_d <= inst_f;
-		pc_d <= pc_f;
-		pc_plus_4_d <= pc_plus_4_f;
+
+
+	always_comb begin
+		if (stall) begin
+			// pipe output back to input to save for next cycle
+			INST_buff_d <= INST_buff_q;
+		end
+		else if (flush) begin
+			INST_buff_d <= '0;
+			INST_D = '0;
+		end
+		else begin
+			INST_D = INST_buff_q;
+			INST_buff_d = INST_F;
+		end
 	end
 endmodule
